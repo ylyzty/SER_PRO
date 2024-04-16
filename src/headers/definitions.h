fb270697fecec8701cb4add5350163f9c5e3fdec
zyli <2436472829@qq.com>
2024-04-16 21:03:44 +0800

    replace the mode of fopen in Solver::toDimacs, wr -> w+

---
+++ src/headers/definitions.h
@@ -0,0 +1,13 @@
+/*******************************************
+ * Created by zyuli on 2024/4/16 
+ *******************************************/
+
+#ifndef SER_PRO_DEFINITIONS_H
+#define SER_PRO_DEFINITIONS_H
+
+#define ERROR_CODE_UNCOMPACT 1
+#define ERROR_CODE_REVERSE_ORDER 2
+#define ERROR_CODE_ABNORMAL_CLAUSES_LENGTH 3
+#define ERROR_CODE_ABNORMAL_LIT 4
+
+#endif //SER_PRO_DEFINITIONS_H
